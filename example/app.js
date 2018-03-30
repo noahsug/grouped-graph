e36a4d6a99066b2d7b2eb7e10ca83d093614a9a3
noah_sugarman <noah.sugarman@airbnb.com>
2018-03-29 21:07:11 -0700

    rename

--- example/app.js
+++ example/app.js
@@ -1,9 +1,7 @@
-import speedyTree from '../src/index'
+import groupedGraph from '../src/index'
 import data from './data.json'
 
 const root = document.querySelector('.visualization')
 
-const visualization = speedyTree.createVisualization(data, {
-  rootNode: 'layout',
-})
+const visualization = groupedGraph.createVisualization(data)
 visualization.attach(root)
